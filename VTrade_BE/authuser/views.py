e039756d8d8b9ef445b9d68b87ed7fc0d9e70e75
Jae Young Seo <jaeyoungseo@jys.local>
2024-11-05 23:35:46 -0500

    adding serializers and starting REST API

--- VTrade_BE/authuser/views.py
+++ VTrade_BE/authuser/views.py
@@ -1,41 +1,10 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import permissions
 from .models import CustomUser
 from .serializers import CustomUserSerializer
-
-class TodoListApiView(APIView):
-    # add permission to check if user is authenticated
-    permission_classes = [permissions.IsAuthenticated]
-
-    # 1. List all
-    def get(self, request, *args, **kwargs):
-        '''
-        List all the users
-        '''
-        users = CustomUser.objects.filter(user = request.user.id)
-        serializer = TodoSerializer(todos, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-    # 2. Create
-    def post(self, request, *args, **kwargs):
-        '''
-        Create the Todo with given todo data
-        '''
-        data = {
-            'task': request.data.get('task'),
-            'completed': request.data.get('completed'),
-            'user': request.user.id
-        }
-        serializer = TodoSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 def login_view(request):
     # Example content for a login view
